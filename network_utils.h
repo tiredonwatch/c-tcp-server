8b697fc06e01f492a1dacd3f1ade93387787dc08
tiredonwatch <169183014+tiredonwatch@users.noreply.github.com>
2026-06-08 02:22:27 +0000

    chore: add Network Config

---
+++ network_utils.h
@@ -0,0 +1,11 @@
+#ifndef NETWORK_UTILS.H
+#define NETWORK_UTILS.H
+
+#include <sys/types.h>
+
+#define SERVER_PORT 8080
+#define BACKLOG     10     // max number of connections in queue
+#define MAX_CLIENTS 10     // total connected clients
+#define BUFFER_SIZE 4096   // 4KB buffer size 
+
+#endif
